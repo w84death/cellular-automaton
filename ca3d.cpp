b107ebc0884562c61cbe5683c02d450dda494230
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-09-07 09:22:29 +0200

    license

--- ca3d.cpp
+++ ca3d.cpp
@@ -5,7 +5,14 @@
 // (c)2015 P1X
 // http://p1x.in
 //
+// Repo:
+// https://github.com/w84death/cellular-automaton
+//
+// Linux:
 // gcc -Os ca3d.cpp -o ca3d.app -lglut -lGL -lGLU -lm
+//
+// OSX:
+// gcc -o ca3d ca3d.cpp -framework GLUT -framework OpenGL
 //
 // ----------------------------------------
 
@@ -574,3 +581,25 @@ int main(int argc, char** argv) {
   glutMainLoop();
    return 0;
 }
+
+
+
+// The MIT License (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
